a7e769bd7e4b28d2db45dca4b2cb943a68f52846
vibhash.chandra <vibhash.chandra@servicenow.com>
2021-05-15 15:41:17 +0530

    updated locust file

--- locustfile.py
+++ locustfile.py
@@ -1,56 +1,68 @@
 # locustfile.py
-from locust import HttpLocust, TaskSet, task
+from locust import HttpUser, TaskSet, task
 import json
 import datetime
 import requests
 
-class UserBehavior(TaskSet):
+# class UserBehavior(TaskSet):
 
-    def on_start(self):
-        self.login()
+#     def on_start(self):
+#         self.login()
 
-    def login(self):
-        # GET login page to get csrftoken from it
-        response = self.client.get('/accounts/login/')
-        csrftoken = response.cookies['csrftoken']
-        # POST to login page with csrftoken
-        self.client.post('/accounts/login/',
-                         {'username': 'suser', 'password': 'asdf1234'}, 
-                         headers={'X-CSRFToken': csrftoken})
+#     def login(self):
+#         # GET login page to get csrftoken from it
+#         response = self.client.get('/accounts/login/')
+#         csrftoken = response.cookies['csrftoken']
+#         # POST to login page with csrftoken
+#         self.client.post('/accounts/login/',
+#                          {'username': 'suser', 'password': 'asdf1234'}, 
+#                          headers={'X-CSRFToken': csrftoken})
+
+#     @task(1)
+#     def index(self):
+#         self.client.get('/')
+
+#     @task(2)
+#     def get_campaign(self):
+#         self.client.get('/5/campaign/')
+
+#     # @task(3)
+#     # def get_stats(self):
+#     #     # ajax GET
+#     #     self.client.get('/5/clientstats/',
+#     #     headers={'X-Requested-With': 'XMLHttpRequest'})
+
+#     @task(3)
+#     def get_advertiser_api(self):
+#         auth_response = self.client.post('/auth/login/', {'username': 'suser', 'password': 'asdf1234'})
+#         auth_token = json.loads(auth_response.text)['token']
+#         jwt_auth_token = 'jwt '+auth_token
+#         adv_api_response = self.client.get('/api/advertiser/', headers={'Authorization': jwt_auth_token})
+#         print("Adv API reponse ", adv_api_response.text)
+
+#     @task(4)
+#     def add_advertiser_api(self):
+#         auth_response = self.client.post('/auth/login/', {'username': 'suser', 'password': 'asdf1234'})
+#         auth_token = json.loads(auth_response.text)['token']
+#         jwt_auth_token = 'jwt '+auth_token
+#         now = datetime.datetime.now()
+        
+#         current_datetime_string = now.strftime("%B %d, %Y")
+#         adv_name = 'locust_adv' 
+#         data = {'name', current_datetime_string}
+#         adv_api_response = requests.post('http://127.0.0.1:8000/api/advertiser/', data, headers={'Authorization': jwt_auth_token})
+
+class WebsiteUser(HttpUser):
 
     @task(1)
     def index(self):
         self.client.get('/')
 
     @task(2)
-    def get_campaign(self):
-        self.client.get('/5/campaign/')
+    def get_people(self):
+        self.client.get('/api/people/1/')
 
-    # @task(3)
-    # def get_stats(self):
-    #     # ajax GET
-    #     self.client.get('/5/clientstats/',
-    #     headers={'X-Requested-With': 'XMLHttpRequest'})
+    @task(2)
+    def get_starships(self):
+        self.client.get('/api/starships/9/')
 
-    @task(3)
-    def get_advertiser_api(self):
-        auth_response = self.client.post('/auth/login/', {'username': 'suser', 'password': 'asdf1234'})
-        auth_token = json.loads(auth_response.text)['token']
-        jwt_auth_token = 'jwt '+auth_token
-        adv_api_response = self.client.get('/api/advertiser/', headers={'Authorization': jwt_auth_token})
-        print("Adv API reponse ", adv_api_response.text)
-
-    @task(4)
-    def add_advertiser_api(self):
-        auth_response = self.client.post('/auth/login/', {'username': 'suser', 'password': 'asdf1234'})
-        auth_token = json.loads(auth_response.text)['token']
-        jwt_auth_token = 'jwt '+auth_token
-        now = datetime.datetime.now()
-        
-        current_datetime_string = now.strftime("%B %d, %Y")
-        adv_name = 'locust_adv' 
-        data = {'name', current_datetime_string}
-        adv_api_response = requests.post('http://127.0.0.1:8000/api/advertiser/', data, headers={'Authorization': jwt_auth_token})
-        
-class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
